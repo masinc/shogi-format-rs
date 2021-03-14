b87c006f0c80310379d7e9b301787f2d6351e9e2
masinc <masinc000@gmail.com>
2021-03-15 02:17:49 +0900

    Default属性を追加

--- src/score/branch.rs
+++ src/score/branch.rs
@@ -1,14 +1,15 @@
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
+#[derive(Default, Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub struct BranchMetadata {
     comment: Option<String>,
     consumed_time: Option<Duration>,
     total_consumed_time: Option<Duration>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
+
+#[derive(Default, Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub struct Branch {
     metadata: BranchMetadata,
     branches: Vec<Branch>,
